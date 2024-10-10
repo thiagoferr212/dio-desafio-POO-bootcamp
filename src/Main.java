e8b3c510af18ba4c30dd35013927a5dc60a741f9
Thiago Ferreira <thiagoferr212@gmail.com>
2024-10-10 08:22:39 -0300

    criação das classees de dominio e conclusão da lógica de negócios.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-package br.com.dio.desafio.dominio;
+
 
 import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args) {
