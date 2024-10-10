96509926989ff843dcf72d9d3af9376960bd2ac5
Thiago Ferreira <thiagoferr212@gmail.com>
2024-10-10 08:55:26 -0300

    conclusão do desafio bootcamp

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -23,10 +25,37 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        // System.out.println(curso1);
+        // System.out.println(curso2);
+        // System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devThiago = new Dev();
+        devThiago.setNome("Thiago");
+        devThiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        devThiago.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Thiago: " + devThiago.getConteudosConcluidos());
+        System.out.println("XP: " + devThiago.calcularTotalXp());
+        
+        System.out.println("---------------");
+        
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos João: " + devJoao.getConteudosConcluidos());
     }
     
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -8,12 +8,8 @@ public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
-
-    public void inscreverBootcamp(Bootcamp bootcamp){
-        this.conteudosInscritos.addAll(bootcamp.getConteudos());
-        bootcamp.getDevsInscritos().add(this);
-    }
-
+    
+    
     public void progredir(){
         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
         if(conteudo.isPresent()){
@@ -24,13 +20,42 @@ public class Dev {
             System.err.println("Você não está matriculado em nenhum conteúdo!");
         }
     }
-
+    
     public double calcularTotalXp(){
         return this.conteudosConcluidos.stream()
         .mapToDouble(conteudo -> conteudo.calcularXp())
         .sum();
     }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+    
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+    
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+    
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
 
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+    
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -67,6 +92,5 @@ public class Dev {
             return false;
         return true;
     }
-    
 
 }
